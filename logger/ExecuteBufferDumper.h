9e7791e7249b317c2b5aed28ce6ed51eb66bb732
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2015-01-10 23:29:35 +0100

    Remove logger
    The logger project has been moved to its own repository.

--- logger/ExecuteBufferDumper.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void DumpExecuteBuffer(LPDIRECT3DEXECUTEBUFFER executeBuffer);
--- logger/targetver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <winsdkver.h>
-
-#define _WIN32_WINNT _WIN32_WINNT_WINXP
-
-#define DIRECTDRAW_VERSION  0x0600
-#define DIRECT3D_VERSION	0x0700
