cb09457f4766d1df77e2888e291aa1686a4ebccb
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2016-02-07 21:41:22 +0100

    Do not needlessly use Windows style paths.
    Using a backslash is actually undefined behaviour
    for C99 or C++03.

--- impl11/ddraw/DeviceResources.cpp
+++ impl11/ddraw/DeviceResources.cpp
@@ -7,21 +7,21 @@
 #include "PrimarySurface.h"
 
 #ifdef _DEBUG
-#include "..\Debug\MainVertexShader.h"
-#include "..\Debug\MainPixelShader.h"
-#include "..\Debug\SmoothScalePixelShader.h"
-#include "..\Debug\VertexShader.h"
-#include "..\Debug\PixelShaderAtestTexture.h"
-#include "..\Debug\PixelShaderTexture.h"
-#include "..\Debug\PixelShaderSolid.h"
+#include "../Debug/MainVertexShader.h"
+#include "../Debug/MainPixelShader.h"
+#include "../Debug/SmoothScalePixelShader.h"
+#include "../Debug/VertexShader.h"
+#include "../Debug/PixelShaderAtestTexture.h"
+#include "../Debug/PixelShaderTexture.h"
+#include "../Debug/PixelShaderSolid.h"
 #else
-#include "..\Release\MainVertexShader.h"
-#include "..\Release\MainPixelShader.h"
-#include "..\Release\SmoothScalePixelShader.h"
-#include "..\Release\VertexShader.h"
-#include "..\Release\PixelShaderAtestTexture.h"
-#include "..\Release\PixelShaderTexture.h"
-#include "..\Release\PixelShaderSolid.h"
+#include "../Release/MainVertexShader.h"
+#include "../Release/MainPixelShader.h"
+#include "../Release/SmoothScalePixelShader.h"
+#include "../Release/VertexShader.h"
+#include "../Release/PixelShaderAtestTexture.h"
+#include "../Release/PixelShaderTexture.h"
+#include "../Release/PixelShaderSolid.h"
 #endif
 
 struct MainVertex
